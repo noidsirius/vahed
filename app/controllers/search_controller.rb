c0e719fa983e082288aa8ab187d20bd186c72062
Noid <navid.saleh.n@gmail.com>
2014-08-21 23:34:23 +0430

    fix search bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,17 +3,21 @@ class SearchController < ApplicationController
 
         @courses = Array.new
         @majors = Major.where("title LIKE :major_title" , {:major_title =>"#{params[:major]}%"})
+        course_title = params[:course_title]
         unless @majors
-            @majors = Array.new
-            @majors << -1
+            #@majors = Array.new
+            #@majors << -1
+            @courses.concat(Course.where("( title LIKE :course_title OR content = :course_title )"  , {:course_title => "%#{course_title}%"}))
+        else
+          @majors.each do |t|
+            #puts "AAAAAAAAA "
+            #puts t.id
+              #@courses.concat(Course.where("( title LIKE :course_title OR content = :course_title ) AND major_id= :major1" , {:course_title => "#{params[:course_title]}%" , :major1=> t.id}))
+              @courses.concat(Course.where("( title LIKE :course_title OR content = :course_title ) AND major_id= :major1"  , {:course_title => "%#{course_title}%" , :major1 => t.id }))
+          end
         end
-        @majors.each do |t|
-          #puts "AAAAAAAAA "
-          #puts t.id
-            #@courses.concat(Course.where("( title LIKE :course_title OR content = :course_title ) AND major_id= :major1" , {:course_title => "#{params[:course_title]}%" , :major1=> t.id}))
-            @courses.concat(Course.where("( title LIKE :course_title OR content = :course_title ) AND major_id= :major1"  , {:course_title => "%#{params[:course_title]}%" , :major1 => t.id }))
-        end
-        @courses=@courses.uniq            
+
+        @courses=@courses.uniq
         #puts "Ssssssssssssss"
         #puts @courses.count
         @units = Array.new
