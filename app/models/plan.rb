d52821b15245f7fede16f0bf85620062e09ccdef
Noid <navid.saleh.n@gmail.com>
2014-08-21 13:16:27 +0430

    fix bug

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -10,7 +10,17 @@ class Plan < ActiveRecord::Base
 	belongs_to :field
 
 
-	def check_all(unit)
+  def sum_unit_num
+    sum = 0
+    units.each do |u|
+      sum += u.unit_num
+    end
+    return sum
+
+  end
+
+
+  def check_all(unit)
 		if(check_multiple_courses(unit))
 			return check_overlap(unit)
 		end
@@ -52,14 +62,5 @@ class Plan < ActiveRecord::Base
 			end
 			return flag
 		end
-end
-
-def sum_unit_num
-  sum = 0
-  units.each do |u|
-    sum += u.unit_num
-  end
-  return sum
-
 end
 
