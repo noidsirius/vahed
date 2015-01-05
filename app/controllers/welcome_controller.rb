ad8f4f4d2188b51cdc7e8f71154780b3a8e28fd5
Noid <navid.saleh.n@gmail.com>
2015-01-05 19:13:22 +0330

    add Contest

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,7 @@ class WelcomeController < ApplicationController
     @vahed = Recruit.where("category = 'Vahed' and user_id = ?", current_user.id).first
     @f1 = Recruit.where("category = 'F1' and user_id = ?", current_user.id).first
     @ar = Recruit.where("category = 'AR' and user_id = ?", current_user.id).first
+    @contest = Recruit.where("category = 'Contest' and user_id = ?", current_user.id).first
 
   end
 end
