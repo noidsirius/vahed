980ddae7a09a1904b2c07fc09290175e51aaa65a
noidsirius <navid.saleh.n@gmail.com>
2015-06-13 15:07:12 +0430

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
       :port                     => 587,
       :domain                   => 'gmail.com',
       :user_name                => 'ut.acm.chapter',
-      :password                 => 'sec0ndgenerati0n',
+      :password                 => '',
       :authentication           => 'plain',
       :enable_starttls_auto     => true
   }
