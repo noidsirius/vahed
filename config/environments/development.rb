6de1c776274078621b15981c21416f7308d6d8e8
Feri73 <ferisampad@gmail.com>
2015-08-16 13:56:56 +0430

    added vahed_dev to root

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_controller.asset_host = "http://localhost:3000"
+  config.action_controller.asset_host = "http://acm.ut.ac.ir/vahed_dev"
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'acm.ut.ac.ir/vahed_dev' }
 
   config.action_mailer.delivery_method = :smtp
 
